e31f8baa4b244fbe5ab242d1b791d6da3e74c48a
Eric Berquist <eric.berquist@gmail.com>
2023-04-24 16:00:46 -0400

    <details>

--- test.bash
+++ test.bash
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+BODY="<details>
+<summary>136.log</details>
+$(<136.log)
+</details>"
+
 gh api \
    --method POST \
    -H "Accept: application/vnd.github+json" \
    -H "X-GitHub-Api-Version: 2022-11-28" \
    /repos/berquist/test-issue-comment-api/issues/1/comments \
-   -f body='Me too'
+   -f body="${BODY}"
